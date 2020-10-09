46a4509f92c4de3555979dac7794e70d51dbb026
halanch599 <altavista5.99>
2020-10-09 16:27:57 +0300

    Add project files.

---
+++ ProjeDemoBIM/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace ProjeDemoBIM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tbEmail = new System.Windows.Forms.TextBox();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.cbRemember = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbEmail
+            // 
+            this.tbEmail.Location = new System.Drawing.Point(225, 136);
+            this.tbEmail.Name = "tbEmail";
+            this.tbEmail.Size = new System.Drawing.Size(408, 28);
+            this.tbEmail.TabIndex = 0;
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(225, 178);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(408, 28);
+            this.tbPassword.TabIndex = 1;
+            this.tbPassword.UseSystemPasswordChar = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.DarkBlue;
+            this.label1.Location = new System.Drawing.Point(157, 143);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 21);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Email :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.DarkBlue;
+            this.label2.Location = new System.Drawing.Point(121, 178);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 21);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Password :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.DarkBlue;
+            this.label3.Location = new System.Drawing.Point(215, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(299, 56);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Log In Here!";
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(221, 112);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(0, 21);
+            this.lblMessage.TabIndex = 6;
+            // 
+            // cbRemember
+            // 
+            this.cbRemember.AutoSize = true;
+            this.cbRemember.ForeColor = System.Drawing.Color.DarkBlue;
+            this.cbRemember.Location = new System.Drawing.Point(225, 222);
+            this.cbRemember.Name = "cbRemember";
+            this.cbRemember.Size = new System.Drawing.Size(165, 25);
+            this.cbRemember.TabIndex = 7;
+            this.cbRemember.Text = "Remember Me?";
+            this.cbRemember.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 12F);
+            this.label4.ForeColor = System.Drawing.Color.OrangeRed;
+            this.label4.Location = new System.Drawing.Point(634, 139);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(19, 23);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "*";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 12F);
+            this.label5.ForeColor = System.Drawing.Color.OrangeRed;
+            this.label5.Location = new System.Drawing.Point(634, 176);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(19, 23);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "*";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.DarkBlue;
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.LightCyan;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Bold);
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Image = global::ProjeDemoBIM.Properties.Resources.clear_symbol_24px_white;
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.Location = new System.Drawing.Point(346, 275);
+            this.button3.Name = "button3";
+            this.button3.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.button3.Size = new System.Drawing.Size(115, 38);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Clear";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkBlue;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.LightCyan;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Bold);
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Image = global::ProjeDemoBIM.Properties.Resources.login_24px_white;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(225, 275);
+            this.button1.Name = "button1";
+            this.button1.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.button1.Size = new System.Drawing.Size(115, 38);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Log In ";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(782, 353);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cbRemember);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.tbEmail);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(800, 400);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(800, 400);
+            this.Name = "Form1";
+            this.Text = "Log In Here";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbEmail;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.CheckBox cbRemember;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ ProjeDemoBIM/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjeDemoBIM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string email = tbEmail.Text.Trim();
+            string password = tbPassword.Text.Trim();
+
+            // imleç
+            lblMessage.Text = "";
+
+            // debug : bug=hata
+            string x = "   Pak   istan   ";
+            string y = x.Trim();
+
+             //trim()
+             //ilk ve son boşlu silecek
+
+            if (email == "")
+            {
+                lblMessage.Text = "Please enter your email.";
+                lblMessage.ForeColor = Color.Red;
+                tbEmail.Focus();
+                return;// donmek
+
+            }
+
+            if (password == "")
+            {
+                lblMessage.Text = "Please enter your password.";
+                lblMessage.ForeColor = Color.Red;
+                tbPassword.Focus();
+                return;
+            }
+
+            if (email=="admin@izu.edu.tr" && password=="123")
+            {
+                lblMessage.Text = "Hosgeldin," + email;
+                lblMessage.ForeColor = Color.Green;
+            }
+            else
+            {
+                lblMessage.Text = "Email or Password is wrong";
+                lblMessage.ForeColor = Color.Red;
+            }
+
+
+
+            //if (email == "")
+            //{
+            //    lblMessage.Text = "Please enter your email.";
+            //    lblMessage.ForeColor = Color.Red;
+            //}
+            //else
+            //{
+            //    if (password == "")
+            //    {
+            //        lblMessage.Text = "Please enter your password.";
+            //        lblMessage.ForeColor = Color.Red;
+            //    }
+            //    else
+            //    {
+            //        lblMessage.Text = "";
+            //    }
+            //}
+
+
+
+            //if (email == "" || password == "")
+            //{
+            //    lblMessage.Text = "Please enter your email/password.";
+            //}
+
+            // ctrl + k + C
+            // Ctrl + k + u
+            //if (email == "")
+            //{
+            //    lblMessage.Text = "Please enter your email.";
+            //}
+
+            //if (password == "")
+            //{
+            //    lblMessage.Text = "Please enter your password.";
+            //}
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            lblMessage.Text = "";
+            tbEmail.Text = "";
+            tbPassword.Text = "";
+            cbRemember.Checked = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            lblMessage.Text = "";
+            tbEmail.Text = "";
+            tbPassword.Text = "";
+            cbRemember.Checked = false;
+        }
+    }
+}
